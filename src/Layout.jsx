9791d85e26442bd91e8f0bac0085289a1e3327c6
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-04-08 22:07:43 +0200

    Refactor layout and add Home page component

---
+++ src/Layout.jsx
@@ -0,0 +1,177 @@
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import previous from "./assets/icons/arrow-previous.svg";
+import next from "./assets/icons/arrow-next.svg";
+import github from "./assets/icons/github.svg";
+import linkedin from "./assets/icons/linkedin.svg";
+import { useState } from "react";
+
+const Layout = () => {
+  const tabs = [
+    {
+      title: "Intro",
+      link: "/",
+    },
+    {
+      title: "Skills",
+      link: "/skills",
+    },
+    {
+      title: "Projects",
+      link: "/projects",
+    },
+    {
+      title: "Contact Me",
+      button: true,
+      link: "/contact",
+    },
+  ];
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [trioPaths, setTrioPaths] = useState({
+    previous: tabs[tabs.length - 1].link,
+    current: "",
+    next: tabs[1].link,
+  });
+
+  const findTrioPaths = (currentPath) => {
+    const paths = tabs.map((tab) => tab.link);
+    const currentIndex = paths.findIndex((path) => path === currentPath);
+    const previousIndex =
+      currentIndex === 0 ? paths.length - 1 : currentIndex - 1;
+    const nextIndex = currentIndex === paths.length - 1 ? 0 : currentIndex + 1;
+
+    setTrioPaths({
+      previous: paths[previousIndex],
+      current: paths[currentIndex],
+      next: paths[nextIndex],
+    });
+  };
+
+  const handleNext = () => {
+    navigate(trioPaths.next);
+    findTrioPaths(trioPaths.next);
+  };
+
+  const handlePrevious = () => {
+    navigate(trioPaths.previous);
+    findTrioPaths(trioPaths.previous);
+  };
+
+  const activeTabStyle = (link) => {
+    return (
+      location.pathname === link &&
+      "border-b-2 border-secondary px-3 py-1 transition-all ease-in-out duration-200 border-secondary"
+    );
+  };
+
+  return (
+    <div className="bg-primary font-[Roboto-Condensed] flex flex-col text-white text-center h-screen relative">
+      <div className="flex sticky top-0 justify-between px-20 py-5  z-50">
+        <div className="flex relative group overflow-hidden flex-col w-[12rem] h-[4rem] items-center justify-center ">
+          <div className="flex cursor-pointer absolute group-hover:translate-y-[5rem] transition-transform duration-700 bottom-0 flex-col space-y-4">
+            <a
+              className="h-[4rem] flex items-center justify-center text-xl px-4 py-1 border"
+              href="https://docs.google.com/document/d/19JvpMsRmWCi8vQI5VoBW8_PIEpVHPvs9/edit?usp=sharing&ouid=110566634388359731224&rtpof=true&sd=true"
+              target="_blank"
+              rel="noopener noreferrer"
+              download
+            >
+              Download CV
+            </a>
+            <div className="h-[4rem] animate-pulse">
+              <div className="h-10 flex items-center text-xl font-bold">
+                <div className=" text-secondary inline-flex">Jean Lionel</div>
+                <div className="bg-secondary ml-1 px-1 min-h-full inline-flex items-center text-primary">
+                  Ndabaga
+                </div>
+              </div>
+              <div className="flex items-center">
+                <span className="border-t border-secondary-light w-full"></span>
+                <span className="whitespace-nowrap mx-4 text-secondary-light">
+                  Full Stack Developer
+                </span>
+                <span className="border-t border-secondary-light w-full"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="lg:space-x-10 text-lg hidden lg:flex items-center">
+          {tabs.map((tab, index) => (
+            <Link
+              key={index}
+              to={tab.link}
+              className={`${
+                tab.button ? "border-2 px-3" : "hover:border-b-4"
+              } py-1 ${activeTabStyle(
+                tab.link
+              )} transition-all ease-in-out duration-200 hover:border-secondary cursor-pointer`}
+            >
+              {tab.title}
+            </Link>
+          ))}
+        </div>
+      </div>
+      <span
+        className="absolute left-10 my-[50vh] z-50"
+        onClick={handlePrevious}
+      >
+        <img className="w-12" src={previous} alt="" />
+      </span>
+
+      <TransitionGroup className="flex-1">
+        <CSSTransition
+          key={location.pathname}
+          timeout={5000}
+          exit={false}
+          classNames="fade"
+        >
+          <Outlet />
+        </CSSTransition>
+      </TransitionGroup>
+      <span className="absolute right-10 my-[50vh] z-50" onClick={handleNext}>
+        <img className="w-12" src={next} alt="" />
+      </span>
+      <div className="px-20 lg:px-0 lg:absolute bottom-8 left-20 flex flex-col text-left space-y-3">
+        <span>
+          <span className="text-secondary">Kigali, Rwanda</span>
+        </span>
+        <span>
+          <a href="tel:+250789912852">+250 789 912 852</a>/{" "}
+          <a href="tel:+250785919170">+250785919170</a>
+        </span>
+        <span>
+          <a
+            href="mailto:ndabagajeanlionel@gmail.com?subject=Mail from My Portfolio"
+            className="text-secondary"
+          >
+            ndabagajeanlionel@gmail.com
+          </a>
+        </span>
+      </div>
+      <div className="flex items-center space-x-4 rotate-90 absolute right-8 bottom-32">
+        <span className="text-secondary">Follow Me</span>
+        <span className="w-10 h-px bg-secondary"></span>
+        <a
+          href="https://github.com/Lionel-hub1"
+          className=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="w-6 -rotate-90" src={github} alt="" />
+        </a>
+        <a
+          className=""
+          href="https://www.linkedin.com/in/jean-lionel-ndabaga-536734213/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="w-6 -rotate-90" src={linkedin} alt="" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import lion from "../assets/images/lionel-one.png";
+
+const Home = () => {
+  
+  return (
+    <div className="flex flex-col lg:flex-row">
+      <div className="flex items-center justify-center max-w-full lg:w-1/2 px-20 relative">
+        <div className="bg-secondary bg-opacity-10 rotate-[38deg] overflow-hidden">
+          <img
+            className="h-[15rem] lg:h-[24rem] -rotate-[30deg] -mb-14  text-wrap opacity-80"
+            src={lion}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="flex w-auto overflow-auto ml-20 lg:ml-0 lg:w-1/2 mr-20 lg:text-3xl items-center">
+        <p className="text-left ">
+          Hello, I&apos;m
+          <span className="text-secondary inline-flex mx-1 font-semibold">
+            Jean Lionel Ndabaga
+          </span>
+          , a full stack developer and a mobile apps developer who
+          <span className="text-danger inline-flex mx-1 font-semibold">
+            care
+          </span>
+          .
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
