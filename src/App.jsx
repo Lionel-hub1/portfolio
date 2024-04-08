4930e01338a97f27a6175b546e62fa9bd0096b8a
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-04-08 22:07:59 +0200

    Refactor routing and add new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import Index from "./pages/Index";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <>
-      <Index />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
