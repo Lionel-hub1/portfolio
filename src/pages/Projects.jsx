7d2e634ec998deea99e6325a41e282f5731884fe
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-04-08 22:07:23 +0200

    Add new pages: Contact, Experience, Projects, and Skills

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+const Projects = () => {
+  return (
+    <section className="py-8">
+      <h2 className="text-2xl font-bold text-center">Projects</h2>
+      {/* Projects I worked on that showcase my skills */}
+    </section>
+  );
+}
+
+export default Projects;
