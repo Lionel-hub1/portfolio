c25c46292dd66df76792a2158deb624f2cdb1f24
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-04-11 23:53:07 +0200

    Update contact form with action attribute and method attribute, then added name attribute to input fields.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,28 +1,53 @@
+import { useState } from "react";
+
 const Contact = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
   return (
     <section className="py-8 flex flex-col items-center">
       <h2 className="text-2xl font-bold text-center pb-5">Contact Me</h2>
-      <form className="w-[70vw] lg:w-1/2 flex flex-col text-primary items-center justify-center">
-        <div className="w-full md:flex md:space-x-4">
-          <input
-            type="text"
-            placeholder="Name"
+      {submitted ? (
+        <p className="text-primary-dark text-center">
+          Thank you for your message. I will get back to you as soon as
+          possible.
+        </p>
+      ) : (
+        <form
+          method="POST"
+          onSubmit={handleSubmit}
+          action="https://getform.io/f/lajkokqb"
+          className="w-[70vw] lg:w-1/2 flex flex-col text-primary items-center justify-center"
+        >
+          <div className="w-full md:flex md:space-x-4">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              className="w-full border-secondary-light border-2 p-2 rounded-md mb-2"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              className="w-full border-secondary-light border-2 p-2 rounded-md mb-2"
+            />
+          </div>
+          <textarea
+            placeholder="Input your message here..."
+            name="message"
             className="w-full border-secondary-light border-2 p-2 rounded-md mb-2"
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            className="w-full border-secondary-light border-2 p-2 rounded-md mb-2"
-          />
-        </div>
-        <textarea
-          placeholder="Message"
-          className="w-full border-secondary-light border-2 p-2 rounded-md mb-2"
-        ></textarea>
-        <button className="w-full bg-secondary  text-primary-dark p-2 rounded-md">
-          Send
-        </button>
-      </form>
+          ></textarea>
+          <button
+            type="submit"
+            className="w-full bg-secondary  text-primary-dark p-2 rounded-md"
+          >
+            Send
+          </button>
+        </form>
+      )}
     </section>
   );
 };
